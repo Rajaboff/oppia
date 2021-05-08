b1711842266882810086a8f04d3ca1e9857cc6cf
Maxim Semerikov <ggurdanikita@gmail.com>
2021-05-08 11:00:31 +0000

    Поднимаем снова на http

--- waiting_up.py
+++ waiting_up.py
@@ -4,13 +4,22 @@ from urllib3 import disable_warnings
 from requests import get
 from time import sleep
 
-URL="https://oqustudy.kz/"
+URL_HTTPS = "https://oqustudy.kz/"
+URL = "http://oqustudy.kz/"
 
-disable_warnings()
-res = get(URL, verify=False)
-print(res.status_code)
 
-while res.status_code != 200:
-    sleep(5)
-    res = get(URL, verify=False)
-    print(res.status_code)
+def main():
+    # status_code = get(URL_HTTPS, verify=False).status_code
+    status_code = 0
+    while status_code != 200:
+        try:
+            disable_warnings()
+
+            sleep(5)
+            status_code = get(URL).status_code
+            print(status_code)
+        except Exception as e:
+            pass
+
+
+main()
